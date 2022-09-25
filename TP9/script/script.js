5204d8252cca57483ff30c08862baef92fd1b816
DaemonWhite <matheo.daemon@gmail.com>
2022-09-25 17:35:50 +0200

    tp9 auto DataGram partial endd

--- TP9/script/script.js
+++ TP9/script/script.js
@@ -46,7 +46,6 @@ function verifHexa(target, valu) {
     let lastValu = valu[valu.length-1];
     if (!lastValu.match(/[0-9|a-f]/gi)){
         valu = valu.substring(0, valu.length-1);
-        console.log('bite', lastValu)
         target.value = "0x" + valu; 
     }
 
@@ -89,7 +88,6 @@ function hexaModif(num){
     let deci = document.getElementById("deci");
     valu = valu.toString();
     valu = valu.replace(/[0]{1}[x]{1}/g, "")
-    console.log(valu)
 
     if (valu == "") {
         return 0;
@@ -195,7 +193,6 @@ function rountedRectangle(canvas, color, text, x, y, width, height, radius) {
         ctx.fill();
         ctx.fillStyle = 'black';
         ctx.font = '20px serif';
-        console.log(width/(20*text.length))
         ctx.fillText(text, x + xText-20*(text.length/2), y + yText);
         ctx.closePath();
     }
@@ -280,17 +277,81 @@ class Rectangle {
     }
 }
 
+class TraceLigne {
+    /**
+     * 
+     * @param {HTMLelement} canvas 
+     * @param {int} x1 
+     * @param {int} y1 
+     * @param {int} x2 
+     * @param {int} y2 
+     * @param {int} size 
+     * @param {Sring} color 
+     */
+    constructor(canvas, x1, y1, x2, y2, size, color){
+        this.canvas = canvas;
+        this.cordonate = Object.assign({}, {
+            xStart: x1,
+            yStart: y1,
+            xEnd: x2,
+            yEnd: y2,
+            size: size
+        })
+
+        this.color = color;
+
+        if (canvas.getContext) {
+            this.ctx = canvas.getContext("2d");
+            this.draw();
+        } else {
+            console.error("Canvas is not define");
+        }
+
+    }
+    /**
+     * @param {String} color
+     */
+    set setColore(color) {
+        this.color = color
+    }
+    /**
+     * @param {int} size
+     */
+    set size(size) {
+        this.cordonate.size = size;
+    }
+
+    draw() {
+        console.log(this.cordonate)
+        this.ctx.beginPath();
+        this.ctx.lineWidth = this.cordonate.size;
+        this.ctx.strokeStyle = this.color;
+        this.ctx.moveTo(this.cordonate.xStart, this.cordonate.yStart);
+        this.ctx.lineTo(this.cordonate.xEnd, this.cordonate.yEnd);
+        this.ctx.stroke();
+        this.ctx.closePath();
+    }
+}
+
 class AutoDatagrame {
     /**
      * 
      * @param {HTMLElement} canvas 
      * @param {Array} rectangle liste rectangle
+     * @param {Array,rectangle} rectangle.x position x
+     * @param {Array,rectangle} rectangle.y position y
+     * @param {Array,rectangle} rectangle.width largeur /2
+     * @param {Array,rectangle} rectangle.height height /2
+     * @param {Array,rectangle} rectangle.radius Rayon
+     * @param {Array,rectangle} rectangle.color Rayon
+     * @param {Array,rectangle} rectangle.text Rayon
      * 
      */
     constructor(canvas, rectangle = []){
         this.canvas = canvas;
+        this.uuid = 0;
         this.rectangle = [];
-        this.traceLigne = []
+        this.traceLignes = [];
         rectangle.forEach(element => {
             this.addRectangle(element.id,
                 element.x,
@@ -300,23 +361,171 @@ class AutoDatagrame {
                 element.radius,
                 element.color,
                 element.text
-                )
+                );
         });
-        this.autoDetectLigne()
+        this.autoDetectLigne();
+    }
+
+    traceLigne() {
+
+    }
+
+    autoDetectLigneCordone(index) {
+        let rect1 = this.rectangle[this.traceLignes[index].rect1];
+        let testHeight = rect1.y + rect1.height*2
+        if (testHeight > this.traceLignes[index].startY) {
+
+            new TraceLigne(this.canvas,
+                this.traceLignes[index].startX,
+                this.traceLignes[index].startY,
+                this.traceLignes[index].endX,
+                this.traceLignes[index].startY,
+                5,
+                "black"
+                )
+
+            new TraceLigne(this.canvas,
+                this.traceLignes[index].endX,
+                this.traceLignes[index].startY,
+                this.traceLignes[index].endX,
+                this.traceLignes[index].endY,
+                    5,
+                    "black"
+                    )
+        } else {
+
+            let startX  =  this.traceLignes[index].startX;
+            let startY  =  this.traceLignes[index].startY;
+            let endX  =  this.traceLignes[index].endX;
+            let endY  =  this.traceLignes[index].endY;
+            
+            let point = {
+                startX: startX,
+                startY: startY,
+                endX: startX,
+                endY: startY + (endY - startY)/2
+            }
+
+            let point2 = {
+                startX: startX,
+                startY: startY + (endY - startY)/2,
+                endX: endX,
+                endY: startY + (endY - startY)/2
+            }
+
+            let point3 = {
+                startX: endX,
+                startY: startY + (endY - startY)/2,
+                endX: endX,
+                endY: endY
+            }
+
+            let tabTeste = [point, point2, point3]
+
+            tabTeste.forEach(element => {
+                console.log(element)
+                new TraceLigne(this.canvas,
+                    element.startX,
+                    element.startY,
+                    element.endX,
+                    element.endY,
+                        5,
+                        "black"
+                        )
+            });
+
+            
+        }
+
+
+    }
+
+    autoDetectPositionLigne(index) {
+        let rect1 = this.rectangle[this.traceLignes[index].rect1];
+        let rect2 = this.rectangle[this.traceLignes[index].rect2];
+
+        let left = rect1.x;
+        let center = rect1.x + rect1.width;
+        let right = rect1.x + rect1.width*2;
+
+        let midHeight = rect1.y + rect1.height;
+
+        let height  = rect1.y + rect1.height*2;
+
+        let position = Object.assign({}, {
+            x: rect2.x + rect2.width,
+            y: rect2.y
+        });
+
+        if (height+20 < position.y) {
+            console.log("Plus Bas");
+
+            this.traceLignes[index].startX = center;
+            this.traceLignes[index].startY = height;
+        } else {
+            console.log("Trop proche");
+            if (left > position.x) {
+                console.log("gauche");
+
+                this.traceLignes[index].startX = left;
+                this.traceLignes[index].startY = midHeight;
+            }
+            if (right < position.x) {
+                console.log("droite");
+
+                this.traceLignes[index].startX = right;
+                this.traceLignes[index].startY = midHeight;
+            }
+            if ( ( left <= position.x ) && ( position.x <= right ) ) {
+                console.log("center");
+
+                this.traceLignes[index].startX = center;
+                this.traceLignes[index].startY = height;
+            }
+        }
+
+        this.traceLignes[index].endX = position.x;
+        this.traceLignes[index].endY = position.y;
+
     }
 
     autoDetectLigne() {
         for (let i = 0; i < this.rectangle.length; i++){
-            for (let i2 = i; i2 < this.rectangle.length; i2++) {
+            for (let i2 = 0; i2 < this.rectangle.length; i2++) {
                 if (this.rectangle[i].id < this.rectangle[i2].id) {
-                    console.log(i, i2)
+                    this.traceLignes.push(Object.assign({},
+                        {
+                            rect1: this.rectangle[i].uuid,    
+                            rect2: this.rectangle[i2].uuid,   
+                        }));
                 }
             }
         }
-    }
 
+        console.log(this.traceLignes)
+
+        for (let i = 0; i < this.traceLignes.length; i++) {
+            this.autoDetectPositionLigne(i);
+            this.autoDetectLigneCordone(i);
+        }
+
+        this.traceLigne()
+        console.log(this.traceLignes)
+    }
+    /**
+     * Ajoute un rectangle
+     * @param {int} id "Definine ss position pour le tracer Ex 0 trace vers 1 mais 0 ne trace pas vers 2"
+     * @param {int} x 
+     * @param {int} y 
+     * @param {int} w Largeur /2
+     * @param {int} h Hauteur /2
+     * @param {int} r 
+     * @param {String} color Nom de la couleur ex: green; red; blue
+     * @param {String} texte 
+     */
     addRectangle(id, x, y, w, h, r, color, texte){
         let eRectangle = Object.assign({},{
+            uuid: this.uuid++,
             id: id,
             x: x,
             y: y,
@@ -326,6 +535,7 @@ class AutoDatagrame {
             color: color,
             text: texte
         });
+
         this.rectangle.push(eRectangle);
         new Rectangle(this.canvas, color, texte, x, y, w, h, r)
     }
@@ -347,7 +557,7 @@ tes = Object.assign({},{
 tes1 = Object.assign({},{
     id: 2,
     x: 220,
-    y: 100,
+    y: 150,
     width: 50,
     height: 100,
     radius: 8,
@@ -366,5 +576,5 @@ tes2 = Object.assign({},{
     text: "lightpink"
 });
 
-arr = [tes, tes1, tes2]
-let e = new AutoDatagrame(rectTEste, arr)
+arr = [tes, tes1, tes2];
+let e = new AutoDatagrame(rectTEste, arr);
